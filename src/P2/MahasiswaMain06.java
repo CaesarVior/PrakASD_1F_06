e283ef178e127b319617a82fa84fb43984698d7a
Caesar Vior <viorcaesar@gmail.com>
2026-02-22 16:29:35 +0700

    P2_2.2 Percobaan 2: Instansiasi Object, serta Mengakses Atribut dan Method

---
+++ src/P2/MahasiswaMain06.java
@@ -0,0 +1,17 @@
+package P2;
+
+public class MahasiswaMain06 {
+    public static void main(String[] args) {
+        Mahasiswa06 mhs1 = new Mahasiswa06();
+        mhs1.nama = "Caesar Vior Byrnanda";
+        mhs1.nim = "254107020055";
+        mhs1.kelas = "TI-1F";
+        mhs1.ipk = 3.65;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("TI-1G");
+        mhs1.updateIpk(3.75);
+        mhs1.tampilkanInformasi();
+    }
+
+}
