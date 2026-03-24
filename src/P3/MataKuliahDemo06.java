64aa1f3aab774794f94d037d2095aa7a072554d8
Caesar Vior <viorcaesar@gmail.com>
2026-03-24 20:54:03 +0700

    P3_Percobaan 1_Percobaan 3

---
+++ src/P3/MataKuliahDemo06.java
@@ -0,0 +1,30 @@
+package P3;
+
+import java.util.Scanner;
+
+public class MataKuliahDemo06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MataKuliah06[] arrayOfMataKuliah = new MataKuliah06[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode          : ");
+            kode = sc.nextLine();
+            System.out.print("Nama          : ");
+            nama = sc.nextLine();
+            System.out.print("Sks           : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam    : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("----------------------------------------");
+
+            arrayOfMataKuliah[i] = new MataKuliah06(kode, nama, sks, jumlahJam);
+
+        }
+    }
+}
