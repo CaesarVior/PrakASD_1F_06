aeb6d97fce17fdd98fc5c623837ff0d65063472e
Caesar Vior Byrnanda <cviorb1@gmail.com>
2026-05-31 18:36:40 +0700

    P11_Percobaan1_Pertanyaan3

--- src/P11/REPORT.md
+++ src/P11/REPORT.md
@@ -31,3 +31,18 @@
 # Hasil Running
 
 ![Screenshot](img/Percobaan1/P11_HasilPercobaan.png)
+
+## Pertanyaan
+
+### 1. Mengapa hasil compile kode program di baris pertama menghasilkan "Linked list kosong"?
+
+Karena pada baris pertama, program baru membuat wadah Linked List baru dan belum ada data mahasiswa sama sekali yang dimasukkan ke dalamnya. Karena datanya masih kosong (head == null), program otomatis mendeteksi kondisi tersebut dan mencetak tulisan "Linked list kosong"
+
+### 2. Jelaskan kegunaan variable temp secara umum pada setiap method!
+
+Variabel temp (atau tmp) berguna sebagai penanda jalan sementara. Karena program harus berjalan lewat depan (head) satu per satu yang dipakai untuk bergeser dari satu data ke data berikutnya tanpa merusak urutan asli Linked List tersebut.
+
+### 3. Lakukan modifikasi agar data dapat ditambahkan dari keyboard!
+
+![Screenshot](img/Percobaan1/P11_HasilModifikasi_Percobaan1.png)
+![Screenshot](img/Percobaan1/P11_HasilPercobaan_Pertanyaan3.png)
--- src/P11/SLLMain06.java
+++ src/P11/SLLMain06.java
@@ -1,27 +1,32 @@
 package P11;
 
+import java.util.Scanner;
+
 public class SLLMain06 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList06 sll = new SingleLinkedList06();
-        
-        Mahasiswa06 mhs1 = new Mahasiswa06("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa06 mhs2 = new Mahasiswa06("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa06 mhs3 = new Mahasiswa06("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa06 mhs4 = new Mahasiswa06("21212203", "Dirga", "4D", 3.6);
 
-        sll.print(); 
-        System.out.println();
+        System.out.println("--- INPUT DATA MAHASISWA ---");
+        System.out.print("Masukkan Nama  : ");
+        String nama = sc.nextLine();
         
-        sll.addFirst(mhs4);
-        sll.print();
-        System.out.println();
+        System.out.print("Masukkan NIM   : ");
+        String nim = sc.nextLine();
         
-        sll.addLast(mhs1);
-        sll.print();
-        System.out.println();
+        System.out.print("Masukkan Kelas : ");
+        String kelas = sc.nextLine();
         
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
+        System.out.print("Masukkan IPK   : ");
+        double ipk = sc.nextDouble();
+
+        Mahasiswa06 mhsBaru = new Mahasiswa06(nim, nama, kelas, ipk);
+
+        sll.addFirst(mhsBaru);
+
+        System.out.println("\n--- HASIL CETAK LINKED LIST ---");
         sll.print();
+        
+        sc.close();
     }
 }
